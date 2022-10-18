4ee7977ef27d4a97bd0798a94774c07d5c63fab6
Breno Barbieri <breno.fbarbieri@gmail.com>
2022-10-18 00:24:07 -0300

    Button and input components created + BMI calculator refactoring and styling in the card component

--- src/Componets/Card.jsx
+++ src/Componets/Card.jsx
@@ -4,7 +4,7 @@ import styles from "./Card.module.css";
 
 const Card = ({ title, children }) => {
 	return (
-		<div className={styles.container}>
+		<div className={"animeLeft " + styles.container}>
 			<h1 className={styles.title}>{title}</h1>
 			{children}
 		</div>
