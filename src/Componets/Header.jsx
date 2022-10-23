2555c38133cde9039a22dd0c91979e7652589368
Breno Barbieri <breno.fbarbieri@gmail.com>
2022-10-23 19:21:26 -0300

    Completed header style

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -5,16 +5,26 @@ import styles from "./Header.module.css";
 const Header = () => {
 	return (
 		<header className={styles.header}>
+			<div className={styles.logo}>
+				<h1>Infinity Digital</h1>
+			</div>
 			<nav className={styles.nav}>
-				<div className={styles.logoContainer}>
-					<h1>Infinity Digital</h1>
-				</div>
-				<span className={styles.credit}>
-					<i>Create by</i>
-					<br />
-					<q>Breno Barbieri</q>
-				</span>
+				<ul>
+					<li>Calculadora</li>
+					<li>Calculadora</li>
+					<li>Calculadora</li>
+				</ul>
 			</nav>
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.linkedin.com/in/brenobarbieri/"
+				className={styles.credit}
+			>
+				<i>Create by</i>
+				<br />
+				<q>Breno Barbieri</q>
+			</a>
 		</header>
 	);
 };
